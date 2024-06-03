1978478fedc22cfa75fcdc999c666b455143daec
niculescsergiu <niculescusergiu00@gmail.com>
2024-06-03 16:49:07 +0200

    Added multiple demo files

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,10 +1,18 @@
 package org.example;
 
 public class Car {
-    //brand
-    // reg.nr
-    //max speed
-    //owner name
-   //horsepower
+    public static String brand =("Brand: BMW");
+    public static String regnumber =("Regnumber: MHO807");
+    public static String Maxspeed =("Maxspeed: 270km/h");
+    public static String Ownername =("Owner name: Sergiu");
+    public static String Horsepower =("Horsepower: 210hp");
+
+    public static void main(String[] args) {
+        System.out.println(Horsepower);
+        System.out.println(Ownername);
+        System.out.println(Maxspeed);
+        System.out.println(regnumber);
+        System.out.println(brand);
+    }
 
 }
---
+++ src/main/java/org/example/score.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class score {
+    public static void main(String[] args) {
+        int score = 75;
+        if (score >= 65);
+        System.out.println("Hurray! You passed!");
+        if (score <= 55-64);
+        System.out.println("You are almost there.");
+        if (score <= 55);
+        System.out.println("Sorry, you did not pass.");
+
+    }
+
+}
