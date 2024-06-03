736ccfed9a00ed52c9ad721711ababf63a0c860b
niculescsergiu <niculescusergiu00@gmail.com>
2024-06-03 13:43:08 +0200

    added new println line

--- src/main/java/org/example/DecimalString.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class DecimalString {
-    double amount = 27.25;
-    double amount2 = 25.25;
-
-
-}
---
+++ src/main/java/org/example/decimals.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class decimals {
+
+
+        static double amount1 = 27.25;
+       static  double amount2 = 25.25;
+       static double result = amount1 + amount2;
+
+
+    public static void main(String[] args){
+        System.out.println(result);
+    }
+}
